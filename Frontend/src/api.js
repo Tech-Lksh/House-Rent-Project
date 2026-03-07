cf18ba673e96476035ecd5c77008ad3d373ed900
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-03-07 13:53:26 +0530

    Fix

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,5 +1,9 @@
-const API_URL = "http://localhost:8001";
+// const API_URL = "http://localhost:8001";
 
-// const API_URL = "https://house-rent-project-5.onrender.com";
+const API_URL = "https://house-rent-project-6.onrender.com";
 
 export default API_URL;
+
+
+
+// mongodb+srv://House:BiYzMZmlFD4rFTPp@cluster0.8aesvbf.mongodb.net/House
