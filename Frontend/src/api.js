a6e24ee5e7bd05ddeca57410f7878316be011b41
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-03-07 18:57:47 +0530

    Backend Url Update

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,9 +1,8 @@
-// const API_URL = "https://house-rent-project-7.onrender.com";
+const API_URL = "https://house-rent-project-7.onrender.com";
 
-const API_URL = "http://localhost:8001";
+// const API_URL = "http://localhost:8001";
 
 export default API_URL;
 
 
 
-// mongodb+srv://House:BiYzMZmlFD4rFTPp@cluster0.8aesvbf.mongodb.net/House
